c96dd4708af5e10a49fe37d091876980b2d14354
pyonemyatkhine <pyonemyatkhine@gmail.com>
2016-11-14 07:18:51 +0000

    rake db:migrate for column type changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114025028) do
+ActiveRecord::Schema.define(version: 20161114071524) do
 
   create_table "channels", force: :cascade do |t|
     t.integer  "creator_id"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20161114025028) do
 
   create_table "chats", force: :cascade do |t|
     t.integer  "channel_id"
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
