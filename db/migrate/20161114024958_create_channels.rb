44e436cc8ba993af0d2a069b3e7955f390dbc992
pyonemyatkhine <pyonemyatkhine@gmail.com>
2016-11-14 02:50:56 +0000

    Update Model

--- db/migrate/20161114024958_create_channels.rb
+++ db/migrate/20161114024958_create_channels.rb
@@ -1,7 +1,7 @@
 class CreateChannels < ActiveRecord::Migration[5.0]
   def change
     create_table :channels do |t|
-      t.int :creator_id
+      t.integer :creator_id
 
       t.timestamps
     end
--- db/migrate/20161114025028_create_chats.rb
+++ db/migrate/20161114025028_create_chats.rb
@@ -1,7 +1,7 @@
 class CreateChats < ActiveRecord::Migration[5.0]
   def change
     create_table :chats do |t|
-      t.int :channel_id
+      t.integer :channel_id
       t.string :content
 
       t.timestamps
