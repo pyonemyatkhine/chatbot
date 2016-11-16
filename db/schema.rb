b8d909b8d9decff46305c8ed9226e275a74534f4
pyonemyatkhine <pyonemyatkhine@gmail.com>
2016-11-16 08:17:49 +0000

    Update Gemfile.lock and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114071524) do
+ActiveRecord::Schema.define(version: 20161116072433) do
 
   create_table "channels", force: :cascade do |t|
     t.integer  "creator_id"
@@ -21,6 +21,20 @@ ActiveRecord::Schema.define(version: 20161114071524) do
   create_table "chats", force: :cascade do |t|
     t.integer  "channel_id"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.integer  "channel_id"
+    t.integer  "member_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
