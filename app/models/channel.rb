54c79a3a8c35d4d32b8ad48903b8c750c3a1953d
pyonemyatkhine <pyonemyatkhine@gmail.com>
2016-11-16 07:55:55 +0000

    Modify Relationship

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,5 @@
 class Channel < ApplicationRecord
+  has_many :chats
+  has_many :members
+  belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,4 @@
 class Member < ApplicationRecord
+  belongs_to :channel
+  belongs_to :member_id, :class_name => 'User', :foreign_key => 'member_id'
 end
