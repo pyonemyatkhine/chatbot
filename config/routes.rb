c0907ea186991bb713632f5b08abc19a8768ebdc
pyonemyatkhine <pyonemyatkhine@gmail.com>
2016-11-16 04:01:12 +0000

    Update routes for Users Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/show'
+  resources :users, only: [:index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
